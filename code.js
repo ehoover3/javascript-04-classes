fe5eebbc9f2a1d42ba642ba815ac631281442ace
Eric Hoover <ehoover3@yahoo.com>
2022-01-23 11:22:37 -0800

    add reset and getAllInfo player methods

--- code.js
+++ code.js
@@ -32,7 +32,29 @@ class Player {
   constructor(n) {
     return (this._name = n);
   }
+
+  reset() {
+    // Start/Restart the Game
+    this._level = "5";
+    this._type = "Peasant";
+    this._weapon = "Saber";
+    this._health = 5;
+  }
+  getAllInfo() {
+    // Returns the Status
+    if (parseInt(this._health) === 0) {
+      this._health = this.name + " Died!";
+      return this.health;
+    } else {
+      return `
+      Name: ${this.name} 
+      Weapon: ${this.weapon}
+      Health: ${this.health}
+        `;
+    }
+  }
 }
 
 let p1 = new Player("Hercules");
-console.log(p1.name);
+console.log(p1.reset());
+console.log(p1.getAllInfo());
